d22cb9efb872e2d54efe0fb052d6f4ff426524f8
didier <didier@d-defrance.fr>
2020-05-18 17:09:39 +0200

    ajout tableau ok

--- tpJsWeb/js/dom_crud.js
+++ tpJsWeb/js/dom_crud.js
@@ -1,7 +1,9 @@
 
 //variables globales:
 var zoneBodyTableau;
-//...
+var zoneCode;
+var zoneNom;
+var zoneChange;
 var tabDevises = [];
 
 window.onload = function(){
@@ -11,6 +13,9 @@ window.onload = function(){
 function initialiserPage(){
 	console.log("initialiserPage");
 	zoneBodyTableau=document.getElementById("bodyTableau");
+	zoneCode=document.getElementById("code");
+	zoneNom=document.getElementById("nom");
+	zoneChange=document.getElementById("change");
 	
 	tabDevises.push({code:'EUR' , nom : 'Euro' , change : 1})
 	tabDevises.push({code:'USD' , nom : 'Dollar' , change : 1.1})
@@ -32,12 +37,12 @@ function initialiserPage(){
 		newRow.insertCell(2).innerHTML = tabDevises[i].change;
 	}
 }
-/*
-function AjoutDevise(){
+
+function ajoutDevise(){
 	//récuperer le contenu des zones saisies (code , nom , change)
-	var valCode = ....;
-	var valNom = ...;
-	var valChange = ...;
+	var valCode = zoneCode.value;
+	var valNom = zoneNom.value;
+	var valChange = Number(zoneChange.value);
 	
 	var nouvelleDevise = {
 	     code : valCode,
@@ -46,8 +51,12 @@ function AjoutDevise(){
 	  } 
 	  
 	//ajout de nouvelleDevise dans le tableau javascript tabDevises
-	//...
+	tabDevises.push(nouvelleDevise);
 	
 	//ajout de nouvelleDevise dans le tableau HTML (partie zoneBodyTableau)
+	var newRow = zoneBodyTableau.insertRow(-1) ;//-1 pour ajout à la fin
+	var newCell1 = newRow.insertCell(0); newCell1.innerHTML = tabDevises[i].code;
+	newRow.insertCell(1).innerHTML = tabDevises[i].nom;
+	newRow.insertCell(2).innerHTML = tabDevises[i].change;
 }
-*/
+

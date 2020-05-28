d827a5229a5ce0310f987a9b30fb90762131e090
didier <didier@d-defrance.fr>
2020-05-28 16:54:23 +0200

    avec produit (en + de devise)

--- tpNodeJs/myNodeServer/server.js
+++ tpNodeJs/myNodeServer/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var deviseApiRoutes = require('./devise-api-routes');
+var produitApiRoutes = require('./produit-api-routes');
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -25,6 +26,7 @@ res.redirect('/html/index.html');
 });
 
 app.use(deviseApiRoutes.apiRouter); //delegate REST API routes to apiRouter(s)
+app.use(produitApiRoutes.apiRouter); 
 
 app.listen(8282 , function () {
 console.log("http://localhost:8282");

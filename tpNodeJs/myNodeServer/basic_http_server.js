7753df8bb4a2ec4e152cffccf6050428ade13cf3
didier <didier@d-defrance.fr>
2020-05-28 14:35:29 +0200

    run_test.bat

--- tpNodeJs/myNodeServer/basic_http_server.js
+++ tpNodeJs/myNodeServer/basic_http_server.js
@@ -2,7 +2,7 @@ var http = require('http');
 //import * as http from 'http'; //es2015 , typescript
 var myHttpFunction = function(req , res ) {
 res.writeHead(200 , {"Content-Type": "text/html"}); //OK=200
-res.write("<html> <body> <i> hello world </i> </body></html>")
+res.write("<html> <body> <i><b> hello world </b></i> </body></html>")
 res.end();
 };
 var server = http.createServer(myHttpFunction);

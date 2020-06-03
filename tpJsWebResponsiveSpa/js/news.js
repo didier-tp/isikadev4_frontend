2e3149fbd5125a2d8f1eb161576f18adc89aa04d
didier <didier@d-defrance.fr>
2020-06-03 11:08:18 +0200

    s

--- tpJsWebResponsiveSpa/js/news.js
+++ tpJsWebResponsiveSpa/js/news.js
@@ -1,5 +1,12 @@
 
+var tabNews = [];
+
 function startNews(){ 
 	console.log("starNews");
-
+    makeAjaxGetRequest("data/news.json" , function(jsonNewsData) {
+		   tabNews=JSON.parse(jsonNewsData);
+		   for(i in tabNews){
+			   console.log(JSON.stringify(tabNews[i]));
+		   }
+	   });
 }

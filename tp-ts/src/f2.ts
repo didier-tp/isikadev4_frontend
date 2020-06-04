e423c3ab86949e8bf48682889e68bd493580f041
didier <didier@d-defrance.fr>
2020-06-04 16:45:44 +0200

    Map

--- tp-ts/src/f2.ts
+++ tp-ts/src/f2.ts
@@ -4,3 +4,16 @@ for(let val of tab1){
     tab2.push(val.toUpperCase());
 }
 console.log(tab2);
+
+/*
+cet exemple necessite 
+     "lib": ["es2015","dom"],
+et   "downlevelIteration": true,  
+  dans tsconfig.json
+*/
+var m = new Map();
+m.set("hiver", "froid , neige");
+m.set("primptemps", "fleur , vert");
+for(let [k,v] of m.entries()){
+    console.log("saison "+ k + " -- " + v);
+    }

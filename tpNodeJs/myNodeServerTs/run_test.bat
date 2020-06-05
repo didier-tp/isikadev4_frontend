a2057431fd293157c6778fe03f6372ab23f27fea
didier <didier@d-defrance.fr>
2020-06-05 15:44:38 +0200

    test

--- tpNodeJs/myNodeServerTs/run_test.bat
+++ tpNodeJs/myNodeServerTs/run_test.bat
@@ -2,4 +2,5 @@ REM lancement (via npm run test ou npm test)
 REM de la partie 
 REM  "scripts": {  "test": "./node_modules/.bin/mocha --reporter spec"  }
 REM de package.json
+set PORT=8282
 npm test > mocha_report.txt 2> err.txt
